9414760e99a5abd792d2d94050ec71808efe3dc7
clpowe <clpowe@gmail.com>
2025-02-28 05:22:01 -0500

    added site-map and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@
 import { defineConfig } from 'astro/config'
 
 import cloudflare from '@astrojs/cloudflare'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true
-		}
-	}),
-	vite: {
-		css: {
-			transformer: 'lightningcss'
-		}
-	}
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true
+        }
+    }),
+
+    vite: {
+        css: {
+            transformer: 'lightningcss'
+        }
+    },
+
+    integrations: [robotsTxt(), sitemap()],
 })
