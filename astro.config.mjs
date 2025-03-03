a8c7c8b9f439000bb4356a4ddd720b7e40e4512c
clpowe <clpowe@gmail.com>
2025-03-03 06:31:05 -0500

    shrug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,23 @@ import { defineConfig } from 'astro/config'
 
 import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from 'astro-robots-txt'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: cloudflare({
-        imageService: 'cloudflare',
-        platformProxy: {
-            enabled: true
-        }
-    }),
+	output: 'server',
+	adapter: cloudflare(),
+	vite: {
+		build: {
+			minify: false
+		},
+		css: {
+			transformer: 'lightningcss'
+		}
+	},
 
-    vite: {
-        build: {
-            minify: false
-        },
-        css: {
-            transformer: 'lightningcss'
-        }
-    },
-
-    integrations: [robotsTxt(), sitemap(), icon()],
-    site: "https://lonnies-mobile-mechanic.pages.dev"
+	integrations: [robotsTxt(), sitemap(), icon()],
+	site: 'https://lonnies-mobile-mechanic.pages.dev'
 })
